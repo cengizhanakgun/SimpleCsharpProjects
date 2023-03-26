173800c3410103910abf4b795ae6d98294a5f204
cengizhanakgun <coderista@gmail.com>
2023-03-26 13:11:17 +0300

    top level statement hatası ile ilgili açıklamalar okuyunuz.

---
+++ SimpleProjects/Top-Level Statements/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+
+
+
+
+//Amacı herhangi main bloğuna ihtiyaç duymadan kodlarımızı derleyebilmektir.
+
+//Hata alıyorsanız aşağıdaki açıklamayı uygulayınız.
+
+//Projeye sağ tıklayalım ve properties kısmındaki target framework kısmını .NET 5.0 yapalım aksi takdirde top level statement desteklenmeyecektir.
+Console.WriteLine("Hello World!");
