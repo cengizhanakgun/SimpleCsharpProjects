a840b1c2370270c7125ad99768457c50cfc4c02d
cengizhanakgun <coderista@gmail.com>
2023-03-26 13:19:35 +0300

    top level statement ile ilgili kurallar açıklaması eklendi.

--- SimpleProjects/Top-Level Statements/Program.cs
+++ SimpleProjects/Top-Level Statements/Program.cs
@@ -9,5 +9,12 @@
 
 //Hata alıyorsanız aşağıdaki açıklamayı uygulayınız.
 
+//Kurallar
+
+//1. using blokları ile namespace blokları arasında kodlar yazılabilir.
+
+//2. bu islem sadece program.cs dosyası içerisinde uygulanabilir.
+
+
 //Projeye sağ tıklayalım ve properties kısmındaki target framework kısmını .NET 5.0 yapalım aksi takdirde top level statement desteklenmeyecektir.
 Console.WriteLine("Hello World!");
