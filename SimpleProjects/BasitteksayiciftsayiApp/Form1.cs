5a786ac9521cc9604479b55f2b6bd33ef4695a3f
cengizhanakgun <coderista@gmail.com>
2023-03-31 00:29:57 +0300

    Tek mi Çift mi  ? Basit Uygulama

---
+++ SimpleProjects/BasitteksayiciftsayiApp/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BasitteksayiciftsayiApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayi = Convert.ToInt32(textBox1.Text);
+            if(sayi % 2 == 0)
+            {
+                label1.Text = "Çift sayı";
+            }
+            else
+            {
+                label1.Text = "Tek sayı";
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
