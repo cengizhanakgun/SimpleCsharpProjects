da3ec1457bc7b3a8d3e873d44905b6093fe0af8d
cengizhanakgun <coderista@gmail.com>
2023-03-30 21:11:25 +0300

    String formatlama form uygulaması

--- SimpleProjects/StringformatlamaFormApp/Form1.Designer.cs
+++ SimpleProjects/StringformatlamaFormApp/Form1.Designer.cs
@@ -28,13 +28,305 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rb0 = new System.Windows.Forms.RadioButton();
+            this.E = new System.Windows.Forms.RadioButton();
+            this.X = new System.Windows.Forms.RadioButton();
+            this.N = new System.Windows.Forms.RadioButton();
+            this.F3 = new System.Windows.Forms.RadioButton();
+            this.F2 = new System.Windows.Forms.RadioButton();
+            this.F1 = new System.Windows.Forms.RadioButton();
+            this.rbDiyez = new System.Windows.Forms.RadioButton();
+            this.D5 = new System.Windows.Forms.RadioButton();
+            this.D3 = new System.Windows.Forms.RadioButton();
+            this.C4 = new System.Windows.Forms.RadioButton();
+            this.C3 = new System.Windows.Forms.RadioButton();
+            this.C2 = new System.Windows.Forms.RadioButton();
+            this.C1 = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.frmatliHali = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(56, 50);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 67);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Formatla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(5, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Sayı";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rb0);
+            this.groupBox1.Controls.Add(this.E);
+            this.groupBox1.Controls.Add(this.X);
+            this.groupBox1.Controls.Add(this.N);
+            this.groupBox1.Controls.Add(this.F3);
+            this.groupBox1.Controls.Add(this.F2);
+            this.groupBox1.Controls.Add(this.F1);
+            this.groupBox1.Controls.Add(this.rbDiyez);
+            this.groupBox1.Controls.Add(this.D5);
+            this.groupBox1.Controls.Add(this.D3);
+            this.groupBox1.Controls.Add(this.C4);
+            this.groupBox1.Controls.Add(this.C3);
+            this.groupBox1.Controls.Add(this.C2);
+            this.groupBox1.Controls.Add(this.C1);
+            this.groupBox1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.Location = new System.Drawing.Point(198, 16);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(255, 205);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Format Türleri";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // rb0
+            // 
+            this.rb0.AutoSize = true;
+            this.rb0.Location = new System.Drawing.Point(125, 169);
+            this.rb0.Name = "rb0";
+            this.rb0.Size = new System.Drawing.Size(112, 23);
+            this.rb0.TabIndex = 16;
+            this.rb0.TabStop = true;
+            this.rb0.Text = "000000.00";
+            this.rb0.UseVisualStyleBackColor = true;
+            // 
+            // E
+            // 
+            this.E.AutoSize = true;
+            this.E.Location = new System.Drawing.Point(125, 146);
+            this.E.Name = "E";
+            this.E.Size = new System.Drawing.Size(37, 23);
+            this.E.TabIndex = 15;
+            this.E.TabStop = true;
+            this.E.Text = "E";
+            this.E.UseVisualStyleBackColor = true;
+            // 
+            // X
+            // 
+            this.X.AutoSize = true;
+            this.X.Location = new System.Drawing.Point(125, 123);
+            this.X.Name = "X";
+            this.X.Size = new System.Drawing.Size(38, 23);
+            this.X.TabIndex = 14;
+            this.X.TabStop = true;
+            this.X.Text = "X";
+            this.X.UseVisualStyleBackColor = true;
+            // 
+            // N
+            // 
+            this.N.AutoSize = true;
+            this.N.Location = new System.Drawing.Point(125, 100);
+            this.N.Name = "N";
+            this.N.Size = new System.Drawing.Size(39, 23);
+            this.N.TabIndex = 13;
+            this.N.TabStop = true;
+            this.N.Text = "N";
+            this.N.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.N.UseVisualStyleBackColor = true;
+            // 
+            // F3
+            // 
+            this.F3.AutoSize = true;
+            this.F3.Location = new System.Drawing.Point(125, 77);
+            this.F3.Name = "F3";
+            this.F3.Size = new System.Drawing.Size(46, 23);
+            this.F3.TabIndex = 12;
+            this.F3.TabStop = true;
+            this.F3.Text = "F3";
+            this.F3.UseVisualStyleBackColor = true;
+            this.F3.CheckedChanged += new System.EventHandler(this.radioButton10_CheckedChanged);
+            // 
+            // F2
+            // 
+            this.F2.AutoSize = true;
+            this.F2.Location = new System.Drawing.Point(125, 54);
+            this.F2.Name = "F2";
+            this.F2.Size = new System.Drawing.Size(46, 23);
+            this.F2.TabIndex = 11;
+            this.F2.TabStop = true;
+            this.F2.Text = "F2";
+            this.F2.UseVisualStyleBackColor = true;
+            // 
+            // F1
+            // 
+            this.F1.AutoSize = true;
+            this.F1.Location = new System.Drawing.Point(125, 31);
+            this.F1.Name = "F1";
+            this.F1.Size = new System.Drawing.Size(46, 23);
+            this.F1.TabIndex = 10;
+            this.F1.TabStop = true;
+            this.F1.Text = "F1";
+            this.F1.UseVisualStyleBackColor = true;
+            this.F1.CheckedChanged += new System.EventHandler(this.radioButton8_CheckedChanged);
+            // 
+            // rbDiyez
+            // 
+            this.rbDiyez.AutoSize = true;
+            this.rbDiyez.Location = new System.Drawing.Point(22, 169);
+            this.rbDiyez.Name = "rbDiyez";
+            this.rbDiyez.Size = new System.Drawing.Size(102, 23);
+            this.rbDiyez.TabIndex = 9;
+            this.rbDiyez.TabStop = true;
+            this.rbDiyez.Text = "###, ##";
+            this.rbDiyez.UseVisualStyleBackColor = true;
+            this.rbDiyez.CheckedChanged += new System.EventHandler(this.radioButton7_CheckedChanged);
+            // 
+            // D5
+            // 
+            this.D5.AutoSize = true;
+            this.D5.Location = new System.Drawing.Point(22, 146);
+            this.D5.Name = "D5";
+            this.D5.Size = new System.Drawing.Size(49, 23);
+            this.D5.TabIndex = 8;
+            this.D5.TabStop = true;
+            this.D5.Text = "D5";
+            this.D5.UseVisualStyleBackColor = true;
+            // 
+            // D3
+            // 
+            this.D3.AutoSize = true;
+            this.D3.Location = new System.Drawing.Point(22, 123);
+            this.D3.Name = "D3";
+            this.D3.Size = new System.Drawing.Size(49, 23);
+            this.D3.TabIndex = 7;
+            this.D3.TabStop = true;
+            this.D3.Text = "D3";
+            this.D3.UseVisualStyleBackColor = true;
+            // 
+            // C4
+            // 
+            this.C4.AutoSize = true;
+            this.C4.Location = new System.Drawing.Point(22, 100);
+            this.C4.Name = "C4";
+            this.C4.Size = new System.Drawing.Size(48, 23);
+            this.C4.TabIndex = 6;
+            this.C4.TabStop = true;
+            this.C4.Text = "C4";
+            this.C4.UseVisualStyleBackColor = true;
+            // 
+            // C3
+            // 
+            this.C3.AutoSize = true;
+            this.C3.Location = new System.Drawing.Point(22, 77);
+            this.C3.Name = "C3";
+            this.C3.Size = new System.Drawing.Size(48, 23);
+            this.C3.TabIndex = 5;
+            this.C3.TabStop = true;
+            this.C3.Text = "C3";
+            this.C3.UseVisualStyleBackColor = true;
+            this.C3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // C2
+            // 
+            this.C2.AutoSize = true;
+            this.C2.Location = new System.Drawing.Point(22, 54);
+            this.C2.Name = "C2";
+            this.C2.Size = new System.Drawing.Size(48, 23);
+            this.C2.TabIndex = 4;
+            this.C2.TabStop = true;
+            this.C2.Text = "C2";
+            this.C2.UseVisualStyleBackColor = true;
+            // 
+            // C1
+            // 
+            this.C1.AutoSize = true;
+            this.C1.Location = new System.Drawing.Point(22, 31);
+            this.C1.Name = "C1";
+            this.C1.Size = new System.Drawing.Size(48, 23);
+            this.C1.TabIndex = 3;
+            this.C1.TabStop = true;
+            this.C1.Text = "C1";
+            this.C1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(56, 16);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(120, 27);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(62, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 19);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Formatlı Hali";
+            // 
+            // frmatliHali
+            // 
+            this.frmatliHali.AutoSize = true;
+            this.frmatliHali.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.frmatliHali.Location = new System.Drawing.Point(62, 191);
+            this.frmatliHali.Name = "frmatliHali";
+            this.frmatliHali.Size = new System.Drawing.Size(55, 19);
+            this.frmatliHali.TabIndex = 5;
+            this.frmatliHali.Text = "frmatli";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(461, 237);
+            this.Controls.Add(this.frmatliHali);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton X;
+        private System.Windows.Forms.RadioButton N;
+        private System.Windows.Forms.RadioButton F3;
+        private System.Windows.Forms.RadioButton F2;
+        private System.Windows.Forms.RadioButton F1;
+        private System.Windows.Forms.RadioButton rbDiyez;
+        private System.Windows.Forms.RadioButton D5;
+        private System.Windows.Forms.RadioButton D3;
+        private System.Windows.Forms.RadioButton C4;
+        private System.Windows.Forms.RadioButton C3;
+        private System.Windows.Forms.RadioButton C2;
+        private System.Windows.Forms.RadioButton C1;
+        private System.Windows.Forms.RadioButton E;
+        private System.Windows.Forms.RadioButton rb0;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label frmatliHali;
     }
 }
 
--- SimpleProjects/StringformatlamaFormApp/Form1.cs
+++ SimpleProjects/StringformatlamaFormApp/Form1.cs
@@ -16,5 +16,71 @@ namespace StringformatlamaFormApp
         {
             InitializeComponent();
         }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton7_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton10_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double sayi = Convert.ToDouble(textBox1.Text);
+
+            if (C1.Checked)
+
+                frmatliHali.Text = String.Format("{0:C1}", sayi);
+            else if (C2.Checked)
+                frmatliHali.Text = String.Format("{0:C2}", sayi);
+            else if (C3.Checked)
+                frmatliHali.Text = String.Format("{0:C3}", sayi);
+            else if (C4.Checked)
+                frmatliHali.Text = String.Format("{0:C4}", sayi);
+            else if (D3.Checked)
+                frmatliHali.Text = String.Format("{0:D3}", sayi.ToString());
+            else if (D5.Checked)
+                frmatliHali.Text = String.Format("{0:D5}", sayi.ToString());
+            else if (F1.Checked)
+                frmatliHali.Text = String.Format("{0:F1}", sayi);
+            else if (F2.Checked)
+                frmatliHali.Text = String.Format("{0:F2}", sayi);
+            else if (F3.Checked)
+                frmatliHali.Text = String.Format("{0:F3}", sayi);
+            else if (N.Checked)
+                frmatliHali.Text = String.Format("{0:N}", sayi);
+            else if (X.Checked)
+                frmatliHali.Text = String.Format("{0:X}", sayi);
+            else if (E.Checked)
+                frmatliHali.Text = String.Format("{0:E}", sayi);
+            else if (rbDiyez.Checked)
+                frmatliHali.Text = String.Format("{0:###.##}", sayi);
+            else if (rb0.Checked)
+                frmatliHali.Text = String.Format("{0:000000.00}", sayi);
+            else frmatliHali.Text = "Herhangi bir format seçilmedi";
+        }
+
+        private void radioButton8_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
