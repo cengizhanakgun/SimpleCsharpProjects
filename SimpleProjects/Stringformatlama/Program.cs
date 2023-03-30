9b9aa402f7f3273295c958fffcbe1f00b527666d
cengizhanakgun <coderista@gmail.com>
2023-03-30 12:26:20 +0300

    Basit string formatlama console uygulaması

---
+++ SimpleProjects/Stringformatlama/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Stringformatlama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int fiyat = 119;
+            int kdv = 18;
+            string yMetin = String.Format("Fiyatı: {0}", fiyat);
+            Console.WriteLine(yMetin);
+
+
+            /// Birden fazla değişkenin kullanılması söz konusu
+            /// 
+
+            Console.WriteLine(String.Format("Ürünün {0} Fiyatına ek kdvsi % {1} kadardır. ",fiyat,kdv));
+        }
+    }
+}
