550645b6b0f468bc7b4ee49642a2e7fa62cab5c6
cengizhanakgun <coderista@gmail.com>
2023-03-31 15:37:58 +0300

    İki sayı ile birden fazla islem yapan app

---
+++ SimpleProjects/İkisayi-ile-islemyapan-basitform-uygulamasi/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace İkisayi_ile_islemyapan_basitform_uygulamasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double sayi1 = Convert.ToDouble(textBox1.Text); 
+            double sayi2 = Convert.ToDouble(textBox2.Text);
+            double sonuc = 0;
+
+            if(comboBox1.Text == "Topla")
+            {
+                sonuc = sayi1 + sayi2;
+
+            }else if(comboBox1.Text == "Çıkar") 
+                sonuc = sayi1- sayi2;
+            else if (comboBox1.Text == "Böl")
+                sonuc = sayi1 / sayi2;
+            else if(comboBox1.Text == "Çarp")
+                sonuc = sayi1 * sayi2;
+            label5.Text = sonuc.ToString();
+
+        }
+    }
+}
