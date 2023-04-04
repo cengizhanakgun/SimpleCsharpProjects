47414b62ddf63e13c0b50cc0ca41e8b60c9debdc
cengizhanakgun <coderista@gmail.com>
2023-04-05 00:14:22 +0300

    Rastgele sayı oluşturma ve listbox'a ekleme

---
+++ SimpleProjects/Birle-20arasinda-sayi-uretme/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Birle_20arasinda_sayi_uretme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Random r = new Random();
+            int rastgeleSayi = r.Next(1, 20);
+            listBox1.Items.Add(rastgeleSayi.ToString());   
+
+        }
+    }
+}
---
+++ SimpleProjects/Birle-20arasinda-sayi-uretme/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Birle_20arasinda_sayi_uretme
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
