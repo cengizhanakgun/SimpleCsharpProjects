7a9ff759ebf7c752a9ae754f97a137c39cb36147
cengizhanakgun <coderista@gmail.com>
2023-04-04 22:01:52 +0300

    Combox'a sayı doldurma uygulaması

---
+++ SimpleProjects/birle-on-arasindaki-sayilari-combox'a-doldurma/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace birle_on_arasindaki_sayilari_combox_a_doldurma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                string sayi = i.ToString()+".Sayı";
+                comboBox1.Items.Add(sayi);
+
+
+            }
+        }
+    }
+}
