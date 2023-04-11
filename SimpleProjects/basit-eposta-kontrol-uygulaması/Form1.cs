ac9fb88e48d7d5fabc4f767cb6e87259b58b1387
Cengizhan Akgun <coderista@gmail.com>
2023-04-11 20:41:57 +0300

    basit eposta kontrol uygulaması

---
+++ SimpleProjects/basit-eposta-kontrol-uygulaması/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace basit_eposta_kontrol_uygulaması
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string stringEmail = textBox1.Text;
+
+            int result = stringEmail.IndexOf("@");
+
+            if (result == -1)
+            {
+                MessageBox.Show("Bu mail adresi doğru değildir.");
+
+            }
+            else
+            {
+                MessageBox.Show("@ işareti doğru pozisyonda bulundu.",result.ToString());
+            }
+        }
+    }
+}
