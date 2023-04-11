f926a9d701df55645750854a57331c057b105f79
Cengizhan Akgun <coderista@gmail.com>
2023-04-11 20:42:33 +0300

    basit cpu tempeture uygulaması

---
+++ SimpleProjects/cpuconsoletemp/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Double CPUtprt = 0;
+System.Management.ManagementObjectSearcher mos = new System.Management.ManagementObjectSearcher(@"root\WMI", "Select * From MSAcpi_ThermalZoneTemperature");
+foreach (System.Management.ManagementObject mo in mos.Get())
+{
+    CPUtprt = Convert.ToDouble(Convert.ToDouble(mo.GetPropertyValue("CurrentTemperature").ToString()) - 2732) / 10;
+    Console.WriteLine("CPU temp : " + CPUtprt.ToString() + " °C");
+}
+Console.ReadKey();
