4fe123c31656eaf96e81ccd93da08821d97bda65
cengizhanakgun <coderista@gmail.com>
2023-04-14 16:21:09 +0300

    büyük harfleri küçüğe küçük harfleri büyüğe çevirme uygulaması

---
+++ SimpleProjects/toupper-tolower-uygulamasi-kucukharfbuyukharf/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace toupper_tolower_uygulamasi_kucukharfbuyukharf
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            int uzunluk = textBox1.Text.Length;
+            string yeniIfade = "";
+            for (int i = 0; i < uzunluk; i++)
+            {
+                if (Char.IsLower(textBox1.Text[i]) == true)
+                    yeniIfade += textBox1.Text[i].ToString().ToUpper();
+                else
+                    yeniIfade += textBox1.Text[i].ToString().ToLower();
+
+                    
+                    
+            }
+            label2.Text = yeniIfade;    
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
