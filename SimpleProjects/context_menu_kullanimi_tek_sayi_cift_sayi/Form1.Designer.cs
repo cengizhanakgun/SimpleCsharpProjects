30ad8121cde8cc80606421972f1c71ed14e492d1
cengizhanakgun <coderista@gmail.com>
2023-04-15 13:43:02 +0300

    Context menu strip kullanımı tek sayı çift sayı

---
+++ SimpleProjects/context_menu_kullanimi_tek_sayi_cift_sayi/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace context_menu_kullanimi_tek_sayi_cift_sayi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.tekÇiftDeğerleriBelirleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listboxaEkleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tekÇiftDeğerleriBelirleToolStripMenuItem,
+            this.listboxaEkleToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(201, 48);
+            
+            // 
+            // tekÇiftDeğerleriBelirleToolStripMenuItem
+            // 
+            this.tekÇiftDeğerleriBelirleToolStripMenuItem.Name = "tekÇiftDeğerleriBelirleToolStripMenuItem";
+            this.tekÇiftDeğerleriBelirleToolStripMenuItem.Size = new System.Drawing.Size(200, 22);
+            this.tekÇiftDeğerleriBelirleToolStripMenuItem.Text = "Tek/Çift Değerleri Belirle";
+            this.tekÇiftDeğerleriBelirleToolStripMenuItem.Click += new System.EventHandler(this.tekÇiftDeğerleriBelirleToolStripMenuItem_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(22, 75);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(200, 244);
+            this.listBox1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(22, 49);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(18, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Sayı";
+            // 
+            // listboxaEkleToolStripMenuItem
+            // 
+            this.listboxaEkleToolStripMenuItem.Name = "listboxaEkleToolStripMenuItem";
+            this.listboxaEkleToolStripMenuItem.Size = new System.Drawing.Size(200, 22);
+            this.listboxaEkleToolStripMenuItem.Text = "Listbox\'a ekle";
+            this.listboxaEkleToolStripMenuItem.Click += new System.EventHandler(this.listboxaEkleToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(245, 335);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem tekÇiftDeğerleriBelirleToolStripMenuItem;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStripMenuItem listboxaEkleToolStripMenuItem;
+    }
+}
+
---
+++ SimpleProjects/context_menu_kullanimi_tek_sayi_cift_sayi/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace context_menu_kullanimi_tek_sayi_cift_sayi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+  
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void tekÇiftDeğerleriBelirleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                int aktifSayi = Convert.ToInt32(listBox1.Items[i]);
+                if (aktifSayi % 2 == 0)
+                {
+                    listBox1.Items[i] = listBox1.Items[i].ToString() + "(Çift Sayı)";
+
+                }
+                else
+                {
+                    listBox1.Items[i] = listBox1.Items[i].ToString() + "(Tek Sayı)";
+                }
+            }
+
+        }
+
+  
+
+        private void listboxaEkleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(textBox1.Text);
+        }
+    }
+}
