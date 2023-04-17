a4c1c1ff587b0415d4e802d649940e2f6a77b9ce
cengizhanakgun <coderista@gmail.com>
2023-04-17 21:10:21 +0300

    istenen değerden başlayıp başka bir değere kadar olan sayıları verir. arttırma sayısı opsiyoneldir.

---
+++ SimpleProjects/istenen-degerden-birbaska-degere-kadarolan-sayıları-yazdırma/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace istenen_degerden_birbaska_degere_kadarolan_sayıları_yazdırma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            textBox4.Text = string.Empty; 
+            int baslangic = Convert.ToInt32(textBox1.Text);
+            int bitis = Convert.ToInt32(textBox2.Text); 
+            int artisMiktari = Convert.ToInt32(textBox3.Text);
+
+            if(bitis > baslangic) {
+
+                for (int i = baslangic; i <= bitis; i=i+artisMiktari)
+
+                {
+
+                    textBox4.Text += i.ToString() + ",";
+
+
+                }
+
+
+            }
+            else
+            {
+                for (int i = baslangic; i>=bitis;i=i+artisMiktari)
+                {
+                    textBox4.Text += i.ToString() + ",";
+                }
+            }
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
